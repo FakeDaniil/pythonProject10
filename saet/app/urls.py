b5fbf9014419df3fc162589b8c35f380c71fd0ec
FakeDaniil <trubicyndaniil513@gmail.com>
2022-07-26 11:11:18 +0300

    First commit

--- saet/app/urls.py
+++ saet/app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('contact', views.contact),
+    path('about_us', views.about_us)
+
 ]
